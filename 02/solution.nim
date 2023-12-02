830816ab45df5bc9f36088a9420f0216d882249d
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-02 12:07:44 +0000

    Added day 2 part 2 solution

--- 02/solution.nim
+++ 02/solution.nim
@@ -25,6 +25,31 @@ proc part1(filename: string): int =
     if valid:
       result += id
 
+proc part2(filename: string): int =
+  for line in lines filename:
+    var
+      bag = findAll(line.split(":")[1], re"\d+ (red|blue|green)")
+      minRed = 0
+      minGreen = 0
+      minBlue = 0
+
+    for cubes in bag:
+      let
+        cubeInfo = cubes.split
+        amount = parseInt(cubeInfo[0])
+      if cubeInfo[1] == "red" and amount > minRed:
+        minRed = amount
+      elif cubeInfo[1] == "green" and amount > minGreen:
+        minGreen = amount
+      elif cubeInfo[1] == "blue" and amount > minBlue:
+        minBlue = amount
+    result += minRed * minGreen * minBlue
+
 
 when isMainModule:
-  echo "Part1 is ", part1("input.txt")
+  let
+    part1Result = part1("input.txt")
+    part2Result = part2("input.txt")
+
+  assert part1Result == 2085
+  assert part2Result == 79315
