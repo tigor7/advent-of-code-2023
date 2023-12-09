40c9c4bbea5091a1c8aad326ce89517beb8bdcf5
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-09 10:58:30 +0000

    Added day 9 part 1 solution

---
+++ 09/solution.nim
@@ -0,0 +1,35 @@
+import ../utils
+import std/[re, strutils, sequtils]
+
+proc checkZero(nums: seq[int]): bool =
+  for num in nums:
+    if num != 0:
+      return false
+  return true
+
+proc rec(nums: seq[int]): int =
+  if checkZero(nums):
+    return 0
+  var diff: seq[int]
+  for i in 1..<nums.len:
+    diff.add(nums[i] - nums[i-1])
+  return nums[^1] + rec(diff)
+
+proc part1(filename: string): int =
+  for line in lines filename:
+    let nums = findAll(line, re"-?\d+").map(proc(x: string): int = parseInt(x))
+    result += rec(nums)
+
+proc part2(filename: string): int =
+  discard
+
+when isMainModule:
+  benchmark "Part 1":
+    let part1Result = part1("input.txt")
+    echo "Part 1 result is ", part1Result
+    assert part1Result == 1934898178
+
+  benchmark "Part 2":
+    let part2Result = part2("test.txt")
+    echo "Part 2 result is ", part2Result
+    # assert part2Result == 254494947
