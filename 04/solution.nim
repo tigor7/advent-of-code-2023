261b2dcd25cf1867f819ad89c40afc31eab99a4a
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-11 21:39:57 +0000

    Refactor day 4

--- 04/solution.nim
+++ 04/solution.nim
@@ -1,3 +1,4 @@
+import ../utils
 import std/[re, strutils, sequtils, math, tables]
 
 type
@@ -6,7 +7,7 @@ type
     winningNums: seq[string]
     cardNums: seq[string]
 
-proc readCardsFromFile(filename: string): seq[Card] =
+iterator cards(filename: string): Card =
   for line in lines filename:
     let
       parsed = line.split(":")
@@ -14,48 +15,37 @@ proc readCardsFromFile(filename: string): seq[Card] =
       nums = parsed[1].split("|")
       winningNums = findAll(nums[0], re"\d+")
       cardNums = findAll(nums[1], re"\d+")
-    result.add(Card(id: id, winningNums: winningNums, cardNums: cardNums))
+    yield Card(id: id, winningNums: winningNums, cardNums: cardNums)
 
 proc getMatches(card: Card): int =
   return filter(card.cardNums, proc(x: string): bool = x in
       card.winningNums).len
 
 proc part1(filename: string): int =
-  let cards = readCardsFromFile(filename)
-  for card in cards:
+  for card in cards filename:
     let matches = getMatches(card)
     if matches > 0:
       result += 2^(matches - 1)
 
 proc part2(filename: string): int =
   var
-    cards = readCardsFromFile(filename)
     copies = initTable[int, int]()
-
-  for card in cards:
-    var
+  for card in cards filename:
+    let
       matches = getMatches(card)
-      cardCopies = 0
-
-    if card.id in copies:
-      cardCopies = copies[card.id]
+      cardCopies = copies.getOrDefault(card.id)
     for i in 1..matches:
-      if card.id + i notin copies:
-        copies[card.id + i] = 1 + cardCopies
-      else:
-        copies[card.id + i] += 1 + cardCopies
+      copies[card.id + i] = copies.getOrDefault(card.id + i) + cardCopies + 1
     result += 1 + cardCopies
 
 
 when isMainModule:
-  let
-    part1TestResult = part1("test.txt")
-    part2TestResult = part2("test.txt")
-    part1Result = part1("input.txt")
-    part2Result = part2("input.txt")
+  benchmark "Part 1":
+    let part1Result = part1("input.txt")
+    echo "Part 1 result is ", part1Result
+    assert part1Result == 26218
 
-  assert part1TestResult == 13
-  assert part2TestResult == 30
-
-  assert part1Result == 26218
-  assert part2Result == 9997537
+  benchmark "Part 2":
+    let part2Result = part2("input.txt")
+    echo "Part 2 result is ", part2Result
+    assert part2Result == 9997537
