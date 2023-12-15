6699a100966431d89a0cffb3701e2e0073fb24c4
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-15 10:11:43 +0000

    Added day 15 part 2 solution

--- 15/solution.nim
+++ 15/solution.nim
@@ -1,12 +1,33 @@
 import ../utils
 import std/[strutils]
 
+type
+  Lens = object
+    label: string
+    len: int
+  Box = object
+    lenses: seq[Lens]
+  Boxes = array[256, Box]
+
+
 proc hash(str: string): int =
   for ch in str:
     result += int(ch)
     result *= 17
     result = result mod 256
 
+proc removeLens(box: var Box, label: string) =
+  for i, lens in box.lenses:
+    if lens.label == label:
+      box.lenses.delete(i)
+      return
+
+proc addLens(box: var Box, label: string, len: int) =
+  for i, lens in box.lenses:
+    if lens.label == label:
+      box.lenses[i].len = len
+      return
+  box.lenses.add(Lens(label: label, len: len))
 
 proc part1(filename: string): int =
   let
@@ -15,9 +36,27 @@ proc part1(filename: string): int =
   for step in steps:
     result += hash(step)
 
-
 proc part2(filename: string): int =
-  discard
+  let
+    f = open(filename)
+    steps = readAll(f).split(",")
+
+  var boxes: Boxes
+  for step in steps:
+    if step[^1] == '-':
+      let
+        label = step[0..<step.len-1]
+        pos = hash(label)
+      removeLens(boxes[pos], label)
+    elif step[^2] == '=':
+      let
+        label = step[0..<step.len-2]
+        pos = hash(label)
+      addLens(boxes[pos], label, parseInt($step[^1]))
+
+  for i, box in boxes:
+    for j, lens in box.lenses:
+      result += (i + 1) * (j + 1) * lens.len
 
 when isMainModule:
   benchmark "Part 1":
@@ -25,6 +64,6 @@ when isMainModule:
     echo "Part 1 result is ", part1Result
     assert part1Result == 501680
   benchmark "Part 2":
-    let part2Result = part2("test.txt")
+    let part2Result = part2("input.txt")
     echo "Part 2 result is ", part2Result
-    # assert part2Result == 0
+    assert part2Result == 241094
