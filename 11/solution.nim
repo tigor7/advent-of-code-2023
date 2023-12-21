dcc172f1945d90cda93a4d8c5d1f45515be161bf
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-21 22:15:14 +0000

    Refactor day 11

--- 11/solution.nim
+++ 11/solution.nim
@@ -1,152 +1,56 @@
 import ../utils
-import std/[strutils, sequtils, deques, tables]
 
-proc readGalaxy(filename: string): seq[seq[char]] =
+type
+  Image = seq[seq[char]]
+  Galaxy = (int, int)
+  Empty = (seq[int], seq[int])
+
+
+proc readImage(filename: string, image: var Image, galaxies: var seq[Galaxy],
+    empty: var Empty) =
+  var i = 0
   for line in lines filename:
-    var inner: seq[char]
-    for ch in line:
+    var
+      inner: seq[char]
+      isEmpty = true
+    for j, ch in line:
+      if ch == '#':
+        isEmpty = false
+        galaxies.add (i, j)
       inner.add(ch)
-    result.add(inner)
+    if isEmpty:
+      empty[0].add(i)
+    image.add(inner)
+    inc i
+  # Check Cols
+  for c in 0..<image[0].len:
+    var isEmpty = true
+    for r in 0..<image.len:
+      if image[r][c] == '#':
+        isEmpty = false
+    if isEmpty:
+      empty[1].add(c)
 
-proc addEmptySpace(galaxy: var seq[seq[char]]) =
-  let emptyHor = newSeq[char](galaxy[0].len).map(proc(x: char): char = '.')
-
-  for r in countdown(galaxy.len-1, 0):
-    var valid = true
-    for c in 0..<galaxy[r].len:
-      if galaxy[r][c] == '#':
-        valid = false
-        break
-    if valid:
-      galaxy.insert(emptyHor, r)
-
-  for c in countdown(galaxy[0].len-1, 0):
-    var valid = true
-    for r in 0..<galaxy.len:
-      if galaxy[r][c] == '#':
-        valid = false
-        break
-    if valid:
-      for r in 0..<galaxy.len:
-        galaxy[r].insert('.', c)
-
-proc checkBounds(i, j, sizeI, sizeJ: int): bool =
-  return i >= 0 and j >= 0 and i < sizeI and j < sizeJ
-
-
-proc bfs(galaxy: seq[seq[char]], processed: seq[(int, int)], i, j: int): int =
+proc part(filename: string, mul: int): int =
   var
-    processed = processed
-    seen: seq[(int, int)]
-    next = Deque[(int, int)]()
-    dis = newTable[(int, int), int]()
-
-  next.addLast (i, j)
-  dis[(i, j)] = 0
-
-  while next.len > 0:
-    let (ii, jj) = next.popFirst
-    # echo (ii, jj)
-    if (ii != i or jj != j) and galaxy[ii][jj] == '#' and (ii, jj) notin
-        processed:
-      # echo "Here: ", (i, j), ": ", (ii, jj)
-      processed.add (ii, jj)
-      result += dis[(ii, jj)]
-    for (r, c) in [(1, 0), (0, 1), (0, -1)]:
-      if checkBounds(ii+r, jj+c, galaxy.len, galaxy[0].len) and (ii+r,
-          jj+c) notin seen:
-        dis[(ii+r, jj+c)] = dis[(ii, jj)] + 1
-        seen.add (ii+r, jj+c)
-        next.addLast((ii+r, jj+c))
-
-proc getEmpty(galaxy: seq[seq[char]], i, j, r, c: int): (int, int) =
-  let
-    fx = min(i, r)
-    sx = max(i, r)
-    fy = min(j, c)
-    sy = max(j, c)
-  for x in fx+1..<sx:
-    var valid = true
-    for y in 0..<galaxy[0].len:
-      # debug(x, y)
-      if galaxy[x][y] == '#':
-        valid = false
-        break
-    if valid:
-      inc result[0]
-
-  for x in fy+1..<sy:
-    var valid = true
-    for y in 0..<galaxy.len:
-      # debug(y, x)
-      if galaxy[y][x] == '#':
-        valid = false
-        break
-    if valid:
-      inc result[1]
-
-proc bfs2(galaxy: seq[seq[char]], processed: seq[(int, int)], i, j: int): int =
-  var
-    processed = processed
-    seen: seq[(int, int)]
-    next = Deque[(int, int)]()
-    dis = newTable[(int, int), int]()
-
-  next.addLast (i, j)
-  dis[(i, j)] = 0
-
-  while next.len > 0:
-    let (ii, jj) = next.popFirst
-    # echo (ii, jj)
-    if (ii != i or jj != j) and galaxy[ii][jj] == '#' and (ii, jj) notin
-        processed:
-      # echo "Here: ", (i, j), ": ", (ii, jj)
-      processed.add (ii, jj)
-      let (d1, d2) = getEmpty(galaxy, i, j, ii, jj)
-      # echo "Dis: ", (abs(ii - i) + d1*9) + (abs(jj - j) + d2*9)
-      # echo "Dis: ", abs(ii - i) + d1 + abs(jj - j) + d2
-      # debug(i, j, ii, jj, d1, d2)
-      # echo ""
-      result += (abs(ii - i) + d1*(1000000-1)) + (abs(jj - j) + d2*(1000000-1))
-    for (r, c) in [(1, 0), (0, 1), (0, -1)]:
-      if checkBounds(ii+r, jj+c, galaxy.len, galaxy[0].len) and (ii+r,
-          jj+c) notin seen:
-        dis[(ii+r, jj+c)] = dis[(ii, jj)] + 1
-        seen.add (ii+r, jj+c)
-        next.addLast((ii+r, jj+c))
-
-proc part1(filename: string): int =
-  var
-    galaxy = readGalaxy(filename)
-    procesed: seq[(int, int)]
-  addEmptySpace(galaxy)
-
-  for i in 0..<galaxy.len:
-    for j in 0..<galaxy[i].len:
-      if galaxy[i][j] == '#':
-        result += bfs(galaxy, procesed, i, j)
-        procesed.add (i, j)
-
-
-
-
-proc part2(filename: string): int =
-  var
-    galaxy = readGalaxy(filename)
-    procesed: seq[(int, int)]
-  for i in 0..<galaxy.len:
-    for j in 0..<galaxy[i].len:
-      if galaxy[i][j] == '#':
-        result += bfs2(galaxy, procesed, i, j)
-        procesed.add (i, j)
+    image: Image
+    galaxies: seq[Galaxy]
+    empty: Empty
+  readImage(filename, image, galaxies, empty)
+  for i, (r1, c1) in galaxies[0..^2]:
+    for (r2, c2) in galaxies[i+1..^1]:
+      for r in min(r1, r2)..<max(r1, r2):
+        result += (if r in empty[0]: mul else: 1)
+      for c in min(c1, c2)..<max(c1, c2):
+        result += (if c in empty[1]: mul else: 1)
 
 when isMainModule:
-  # benchmark "Part 1":
-  #   let part1Result = part1("input.txt")
-  #   echo "Part 1 result is ", part1Result
-  #   # assert part1Result == 1934898178
+  benchmark "Part 1":
+    let part1Result = part("input.txt", 2)
+    echo "Part 1 result is ", part1Result
+    assert part1Result == 9623138
   echo ""
   benchmark "Part 2":
-    let part2Result = part2("input.txt")
+    let part2Result = part("input.txt", 1000000)
     echo "Part 2 result is ", part2Result
-    # assert part2Result == 1129
+    assert part2Result == 726820169514
