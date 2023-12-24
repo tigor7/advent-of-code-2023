2c2cca2a71c4915afee03bfb2b095405fd290b01
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-24 17:28:36 +0000

    Added day 18 part 2 solution

--- 18/solution.nim
+++ 18/solution.nim
@@ -33,6 +33,32 @@ proc readPoints(filename: string): MapInfo =
     else:
       discard
 
+proc readPoints2(filename: string): MapInfo =
+  var last = (0, 0)
+  for line in lines filename:
+    let
+      n = parseHexInt(line.split()[2][2..^3])
+    result.size += n
+    case line[^2]:
+    of '0':
+      let added = last + (0, n)
+      result.points.add added
+      last = added
+    of '2':
+      let added = last - (0, n)
+      result.points.add added
+      last = added
+    of '3':
+      let added = last - (n, 0)
+      result.points.add added
+      last = added
+    of '1':
+      let added = last + (n, 0)
+      result.points.add added
+      last = added
+    else:
+      discard
+
 proc part1(filename: string): int =
   let
     mapInfo = readPoints(filename)
@@ -52,7 +78,22 @@ proc part1(filename: string): int =
   result = size + inner
 
 proc part2(filename: string): int =
-  discard
+  let
+    mapInfo = readPoints2(filename)
+    size = mapInfo.size
+    points = mapInfo.points
+  var det = 0
+  for i in 0 ..< points.len:
+    var
+      c1 = i
+      c2 = i + 1
+    if c1 == points.len - 1:
+      c2 = 0
+    det += points[c1][0] * points[c2][1] - points[c2][0] * points[c1][1]
+  let
+    shoelace = abs(det) div 2
+    inner = shoelace - (size div 2) + 1
+  result = size + inner
 
 when isMainModule:
   benchmark "Part 1":
@@ -61,6 +102,6 @@ when isMainModule:
     assert part1Result == 40714
   echo ""
   benchmark "Part 2":
-    let part2Result = part2("test.txt")
+    let part2Result = part2("input.txt")
     echo "Part 2 result is ", part2Result
-    # assert part2Result == 0
+    assert part2Result == 129849166997110
