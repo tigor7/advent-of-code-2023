99023b051448bd5b38d49b2ed4358db337843568
Santiago Pujol <santiagopujol7@gmail.com>
2023-12-24 17:12:49 +0000

    Added day 18 part 1 solution

---
+++ 18/solution.nim
@@ -0,0 +1,66 @@
+import ../utils
+import std/strutils
+
+type
+  Point = (int, int)
+  MapInfo = object
+    points: seq[Point]
+    size: int
+
+proc readPoints(filename: string): MapInfo =
+  var last = (0, 0)
+  for line in lines filename:
+    let
+      n = parseInt(line.split()[1])
+    result.size += n
+    case line[0]:
+    of 'R':
+      let added = last + (0, n)
+      result.points.add added
+      last = added
+    of 'L':
+      let added = last - (0, n)
+      result.points.add added
+      last = added
+    of 'U':
+      let added = last - (n, 0)
+      result.points.add added
+      last = added
+    of 'D':
+      let added = last + (n, 0)
+      result.points.add added
+      last = added
+    else:
+      discard
+
+proc part1(filename: string): int =
+  let
+    mapInfo = readPoints(filename)
+    size = mapInfo.size
+    points = mapInfo.points
+  var det = 0
+  for i in 0 ..< points.len:
+    var
+      c1 = i
+      c2 = i + 1
+    if c1 == points.len - 1:
+      c2 = 0
+    det += points[c1][0] * points[c2][1] - points[c2][0] * points[c1][1]
+  let
+    shoelace = abs(det) div 2
+    inner = shoelace - (size div 2) + 1
+  result = size + inner
+
+proc part2(filename: string): int =
+  discard
+
+when isMainModule:
+  benchmark "Part 1":
+    let part1Result = part1("input.txt")
+    echo "Part 1 result is ", part1Result
+    assert part1Result == 40714
+  echo ""
+  benchmark "Part 2":
+    let part2Result = part2("test.txt")
+    echo "Part 2 result is ", part2Result
+    # assert part2Result == 0
